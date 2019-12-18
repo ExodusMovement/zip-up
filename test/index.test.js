edc6ddbd2436c4f69253bd74ecee90d8f8366378
R. Douglas Barbieri <doug@dooglio.net>
2019-12-18 15:08:18 -0600

    Added improved testing.
    However, the last test, which tests the excluded dirs,
    still fails.

--- test/index.test.js
+++ test/index.test.js
@@ -4,75 +4,133 @@ const Unzip = require('node-stream-zip')
 
 const Zip = require('../bundle')
 
-test('index: create zip file', async (t) => {
-  const zipfile = './test/output.zip'
+async function doesNotReject (t, promise, msg) {
+  t.doesNotReject(promise, null, msg)
+  return promise
+}
 
+const zipfile = './test/output.zip'
+
+async function createZip () {
+  if (await fs.exists(zipfile)) {
+    await fs.unlink(zipfile)
+  }
+  return new Zip(zipfile, { level: 1 })
+}
+
+function createUnzip () {
+  return new Promise((resolve, reject) => {
+    const unzip = new Unzip({
+      file: zipfile,
+      storeEntries: true,
+      skipEntryNameValidation: true // if there are backslashes, ignore them
+    })
+
+    unzip.on('error', (err) => {
+      reject(err)
+    })
+
+    unzip.on('ready', async () => {
+      resolve(unzip)
+    })
+  })
+}
+
+test('index: create zip file', async t => {
   t.pass('Reading directory "fixtures"')
 
-  async function createZip () {
-    if (await fs.exists(zipfile)) {
-      await t.doesNotReject(fs.unlink(zipfile), null, 'zipfile unlinks')
+  let zipper
+
+  t.test('zip fixtures, no hidden dir', async t => {
+    try {
+      zipper = await doesNotReject(t, createZip(), 'createZip does not reject')
+      await zipper.addDir('test/fixtures', 'test/fixtures', {
+        ignoreHidden: true
+      })
+      const written = await doesNotReject(
+        t,
+        zipper.finalize(),
+        'Finalize without rejection'
+      )
+      t.pass(written + ' total bytes written')
+
+      const unzip = await doesNotReject(
+        t,
+        createUnzip(),
+        'create unzip without rejection'
+      )
+      const files = Object.keys(unzip.entries()).sort()
+      t.deepEqual(
+        files,
+        ['test/fixtures/a.txt', 'test/fixtures/b/c.txt', 'test/fixtures/f/g.txt'],
+        'should be no hidden folder'
+      )
+      t.end()
+    } catch (err) {
+      // If this throws, we still need to catch the exception otherwise it's unhandled.
     }
-    return new Zip(zipfile, { level: 1 })
-  }
+  })
 
-  function createUnzip () {
-    return new Promise((resolve, reject) => {
-      const unzip = new Unzip({
-        file: zipfile,
-        storeEntries: true,
-        skipEntryNameValidation: true // if there are backslashes, ignore them
-      })
+  t.test('add hidden dir', async t => {
+    try {
+      await zipper.addDir('test/fixtures/.d', 'test/fixtures/.d')
+      const written = await doesNotReject(
+        t,
+        zipper.finalize(),
+        'Finalize without rejection'
+      )
+      t.pass(written + ' total bytes written')
 
-      unzip.on('error', (err) => {
-        console.log('error found!')
-        console.error(err)
-        reject(err)
-      })
-
-      unzip.on('ready', async () => {
-        resolve(unzip)
-      })
-    })
-  }
-
-  const zipper = await createZip()
-
-  t.test('zip fixtures, no hidden dir', async (t) => {
-    await zipper.addDir('test/fixtures', 'test/fixtures', { ignoreHidden: true })
-    const written = await zipper.finalize()
-    t.pass(written + ' total bytes written')
-
-    const unzip = await createUnzip()
-    const files = Object.keys(unzip.entries()).sort()
-    t.deepEqual(files, ['test/fixtures/a.txt', 'test/fixtures/b/c.txt'], 'should be no hidden folder')
+      const unzip = await doesNotReject(
+        t,
+        createUnzip(),
+        'create unzip without rejection'
+      )
+      const files = Object.keys(unzip.entries()).sort()
+      t.deepEqual(
+        files,
+        [
+          'test/fixtures/.d/e.txt',
+          'test/fixtures/a.txt',
+          'test/fixtures/b/c.txt',
+          'test/fixtures/f/g.txt'
+        ],
+        'all files including hidden folder'
+      )
+    } catch (err) {
+      // If this throws, we still need to catch the exception otherwise it's unhandled.
+    }
     t.end()
   })
 
-  t.test('zip fixtures with hidden dir', async (t) => {
-    await zipper.addDir('test/fixtures/.d', 'test/fixtures/.d', { ignoreHidden: false })
-    const hiddenWritten = await zipper.finalize()
-    t.pass(hiddenWritten + ' total bytes written')
+  t.test('zip fixtures excluded dir and hidden', async t => {
+    try {
+      zipper = await doesNotReject(t, createZip(), 'createZip does not reject')
+      await zipper.addDir('test/fixtures', 'test/fixtures', {
+        ignoreHidden: true,
+        excludeDirectories: 'test/fixtures/f'
+      })
+      const written = await doesNotReject(
+        t,
+        zipper.finalize(),
+        'Finalize without rejection'
+      )
+      t.pass(written + ' total bytes written')
 
-    const unzip = await createUnzip()
-    const files = Object.keys(unzip.entries()).sort()
-    t.deepEqual(files, ['test/fixtures/.d/e.txt', 'test/fixtures/a.txt', 'test/fixtures/b/c.txt'], 'all files including hidden folder')
+      const unzip = await doesNotReject(
+        t,
+        createUnzip(),
+        'create unzip without rejection'
+      )
+      const files = Object.keys(unzip.entries()).sort()
+      t.deepEqual(
+        files,
+        ['test/fixtures/a.txt', 'test/fixtures/b/c.txt'],
+        'all files except hidden and ignored dir'
+      )
+    } catch (err) {
+      // If this throws, we still need to catch the exception otherwise it's unhandled.
+    }
     t.end()
   })
-
-  t.test("zip fixtures excluding dir", async t => {
-    const zipper = new Zip(zipfile, { level: 1 });
-    await zipper.addDir("test/fixtures", "test/fixtures", {
-      ignoreHidden: true,
-      excludeDirectories: "test/fixtures/f"
-    });
-    const written = await zipper.finalize();
-    t.pass(written + " total bytes written");
-
-    const unzip = await createUnzip()
-    const files = Object.keys(unzip.entries()).sort()
-    t.deepEqual(files, ['test/fixtures/a.txt', 'test/fixtures/b/c.txt'], 'all files including hidden folder')
-    t.end()
-  });
-
 })
